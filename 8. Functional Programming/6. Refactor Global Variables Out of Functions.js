c930e6f897acd3c70a8e58dabe1158a348af39f1
GregVaz <grvazquezya@ittepic.edu.mx>
2020-04-01 01:32:56 -0700

    Modulo 8 Functional Programming

---
+++ 8. Functional Programming/6. Refactor Global Variables Out of Functions.js
@@ -0,0 +1,69 @@
+/*
+So far, we have seen two distinct principles for functional programming:
+
+1) Don't alter a variable or object - create new variables and objects and return them if need be from a function.
+
+2) Declare function arguments - any computation inside a function depends only on the arguments, and not on any global object or variable.
+
+Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.
+*/
+
+//Instruction
+//Rewrite the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of the array passed to it and return a new array (list). The remove function should remove the given bookName from the array passed to it.
+
+
+// The global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add (bookList, bookName) {
+  let list = [...bookList];
+  
+  list.push(bookName);
+  return list;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove (bookList, bookName) {
+  let list = [...bookList];
+
+  if (list.indexOf(bookName) >= 0) {
+
+    list.splice(list.indexOf(bookName), 1);
+    return list;
+
+    // Change code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+
+
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before bookName
+
+function add(list, bookName) {
+  return [...list, bookName];
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+function remove(list, bookName) {
+  return list.filter(book => book !== bookName);
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
